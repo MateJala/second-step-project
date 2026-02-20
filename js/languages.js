4f96445aeccb65a8740494ee6cce0535b77875b9
Mate Jala <m.jalagonia2009@gmail.com>
2026-02-20 19:30:49 +0400

    small css changes

--- js/languages.js
+++ js/languages.js
@@ -14,6 +14,14 @@ const translations = {
         stayPay : "Stay First, Pay After!",
         hour24: "24 Hour Restaurant",
         spa : "Spa Included!",
+        hotel: "Hotel",
+        room: "Room",
+        customer: "Customer",
+        status: "Status",
+        checkIn: "Check in",
+        checkOut: "Check out",
+        total: "Total Price",
+        actions: "Actions",
     },
 
     ka : {
@@ -31,6 +39,14 @@ const translations = {
         stayPay : "ჯერ დარჩი, გადაიხადე მერე!",
         hour24: "24 საათიანი რესტორანი",
         spa : "სპა შედის!",
+        hotel: "სასტუმრო",
+        room: "ოთახი",
+        customer: "მომხმარებელი",
+        status: "სტატუსი",
+        checkIn: "შესვლა",
+        checkOut: "გასვლა",
+        total: "მთლიანი ფასი",
+        actions: "მოქმედებები",
     },
 
     ru : {
@@ -48,6 +64,14 @@ const translations = {
         stayPay : "Сначала останься, заплати потом!",
         hour24: "Ресторан круглосуточно",
         spa : "Спа включено!",
+        hotel: "Отель",
+        room: "Номер",
+        customer: "Клиент",
+        status: "Статус",
+        checkIn: "Заезд",
+        checkOut: "Выезд",
+        total: "Общая стоимость",
+        actions: "Действия",
     },
 
     sp : {
@@ -65,6 +89,14 @@ const translations = {
         stayPay : "¡Quédate primero, paga después!",
         hour24: "Restaurante 24 horas",
         spa : "¡Spa incluido!",
+        hotel: "Hotel",
+        room: "Habitación",
+        customer: "Cliente",
+        status: "Estado",
+        checkIn: "Entrada",
+        checkOut: "Salida",
+        total: "Precio total",
+        actions: "Acciones",
     }
 };
 
